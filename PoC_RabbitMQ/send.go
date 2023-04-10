d58c81209bb6e2b0133c3cda05f88184cf24d581
Evers419 <78910809+Evers419@users.noreply.github.com>
2023-04-10 17:41:26 +0200

    rabbitmq poc start

---
+++ PoC_RabbitMQ/send.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"time"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	failOnError(err, "failed to connect to rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "failed to open channel")
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "hello world"
+	err = ch.PublishWithContext(ctx,
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require google.golang.org/protobuf v1.30.0
 
 require (
+	github.com/rabbitmq/amqp091-go v1.8.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
